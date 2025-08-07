4808053ca6c513a76dab0ce72db41387ee5bc8ad
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-08-07 15:27:07 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import UserProfile from "./components/user/UserProfile";
 import { Toaster } from "sonner";
 
 const App = () => {
-  const [userName, setUserName] = useState("noto");
+  const [userName, setUserName] = useState("freeCodeCamp");
   return (
     <main className="mx-auto max-w-6xl px-8 py-20">
       <SearchForm userName={userName} setUserName={setUserName} />
