c0fb242859cffa450c4f84f75ed96a797c45ed1a
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-08-07 13:21:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import SearchForm from "./components/form/SearchForm";
+import UserProfile from "./components/user/UserProfile";
+import { Toaster } from "sonner";
+
+const App = () => {
+  const [userName, setUserName] = useState("noto");
+  return (
+    <main className="mx-auto max-w-6xl px-8 py-20">
+      <SearchForm userName={userName} setUserName={setUserName} />
+      <UserProfile userName={userName} />
+      <Toaster />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/user/UserProfile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+type UserProfileProps = {
+  userName: string;
+};
+
+const UserProfile = ({ userName }: UserProfileProps) => {
+  return <h1 className="text-2xl font-bold">{userName}</h1>;
+};
+
+export default UserProfile;
